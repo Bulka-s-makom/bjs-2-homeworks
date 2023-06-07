1b35d72b6b4e7e3bf8f83ffa9ddb40b28251bd3f
Neto test <fin_parniska1997@mail.ru>
2023-06-07 19:32:16 +0500

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.hasOwnProperty('marks')){
+        this.marks.push(...marksToAdd)
+      } else {
+        return 0;
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty('marks') && this.marks.length > 0) {
+        let result = this.marks.reduce((acc, curr) => acc+curr);
+        return result/this.marks.length;
+      } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
